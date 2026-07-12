b639a480f7cee5bceb36118335f4778481270c54
Siddhant Jain <siddhj2206@gmail.com>
2026-07-12 18:28:44 +0530

    Add Phase 2A generation handoff module

---
+++ src/spatial_ingestion/generation_handoff/__init__.py
@@ -0,0 +1,15 @@
+from spatial_ingestion.generation_handoff.assembler import GenerationHandoffBuilder
+from spatial_ingestion.generation_handoff.models import (
+    GenerationHandoff,
+    GenerationMode,
+    HandoffFrame,
+    SyncViewGroup,
+)
+
+__all__ = [
+    "GenerationHandoff",
+    "GenerationHandoffBuilder",
+    "GenerationMode",
+    "HandoffFrame",
+    "SyncViewGroup",
+]
